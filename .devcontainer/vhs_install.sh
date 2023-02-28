bfce346af254d90f530ff46183c61b75af0addd8
Patrick Coxall <mr.coxall@mths.ca>
2023-02-28 18:38:16 +0000

    update before reboot

--- .devcontainer/vhs_install.sh
+++ .devcontainer/vhs_install.sh
@@ -5,11 +5,14 @@ sudo apt update
 sudo apt -y full-upgrade
 
 # install prerequisites
-sudo apt install ffmpeg
-sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev
+sudo apt install -y ffmpeg
+sudo apt-get install -y build-essential cmake git libjson-c-dev libwebsockets-dev
 git clone https://github.com/tsl0922/ttyd.git
 cd ttyd && mkdir build && cd build
 cmake ..
 make && sudo make install
 
 go install github.com/charmbracelet/vhs@latest
+
+# remove ttyd files
+sudo rm -r ./ttyd
